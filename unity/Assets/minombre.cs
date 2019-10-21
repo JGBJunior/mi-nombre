0c4b74b884f9ee41a58f2729c9a243eeaced4124
JGBJunior <jgbjunior99@gmail.com>
2019-10-21 20:24:07 +0200

    ggunity

---
+++ unity/Assets/minombre.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minombre : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("javier gonzalez benitez");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("19");
+	}
+}
